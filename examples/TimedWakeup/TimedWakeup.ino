02e68ddd0fc673d8ede7d4e11b003cd5b6503707
fpr <fabien.perroquin@wi6labs.com>
2017-12-11 17:00:53 +0100

    Add examples
    Signed-off-by: fpr <fabien.perroquin@wi6labs.com>

---
+++ examples/TimedWakeup/TimedWakeup.ino
@@ -0,0 +1,24 @@
+/*
+  TimedWakeup
+
+  This sketch demonstrates the usage of Internal Interrupts to wakeup a chip in deep sleep mode.
+
+  In this sketch, the internal RTC will wake up the processor every 2 seconds.
+
+  This example code is in the public domain.
+*/
+
+#include "STM32LowPower.h"
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  // Configure low power
+  LowPower.begin();
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  LowPower.deepSleep(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  LowPower.deepSleep(500);
+}
